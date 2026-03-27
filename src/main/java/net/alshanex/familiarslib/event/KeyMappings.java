25dfbf1ae3d54b49edb20e0f31da86f5625ea205
Alshanex <discordpermacc02@gmail.com>
2026-03-27 20:18:26 +0100

    Fixed keybinds

--- src/main/java/net/alshanex/familiarslib/event/KeyMappings.java
+++ src/main/java/net/alshanex/familiarslib/event/KeyMappings.java
@@ -1,6 +1,7 @@
 package net.alshanex.familiarslib.event;
 
 import com.mojang.blaze3d.platform.InputConstants;
+import io.redspace.ironsspellbooks.player.ExtendedKeyMapping;
 import net.alshanex.familiarslib.FamiliarsLib;
 import net.minecraft.client.KeyMapping;
 import net.neoforged.api.distmarker.Dist;
@@ -12,8 +13,8 @@ import net.neoforged.neoforge.client.settings.KeyConflictContext;
 @EventBusSubscriber(value = Dist.CLIENT, modid = FamiliarsLib.MODID, bus = EventBusSubscriber.Bus.MOD)
 public class KeyMappings {
     public static final String KEY_BIND_SUMMONING_CATEGORY= "key.familiarslib.summoning_keys";
-    public static final KeyMapping SUMMONING_KEYMAP = new KeyMapping(getResourceName("summoning_key"), KeyConflictContext.IN_GAME, InputConstants.Type.KEYSYM, InputConstants.KEY_X, KEY_BIND_SUMMONING_CATEGORY);
-    public static final KeyMapping SCREEN_KEYMAP = new KeyMapping(getResourceName("screen_key"), KeyConflictContext.IN_GAME, InputConstants.Type.KEYSYM, InputConstants.KEY_H, KEY_BIND_SUMMONING_CATEGORY);
+    public static final ExtendedKeyMapping SUMMONING_KEYMAP = new ExtendedKeyMapping(getResourceName("summoning_key"), KeyConflictContext.IN_GAME, InputConstants.Type.KEYSYM, InputConstants.KEY_X, KEY_BIND_SUMMONING_CATEGORY);
+    public static final ExtendedKeyMapping SCREEN_KEYMAP = new ExtendedKeyMapping(getResourceName("screen_key"), KeyConflictContext.IN_GAME, InputConstants.Type.KEYSYM, InputConstants.KEY_H, KEY_BIND_SUMMONING_CATEGORY);
 
     private static String getResourceName(String name) {
         return String.format("key.familiarslib.%s", name);
